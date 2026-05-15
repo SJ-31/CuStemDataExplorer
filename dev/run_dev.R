7642a00c593ee0375de0ed1b5541783fbdc0bf34
Shann Chongwattanankul <shanj3131@gmail.com>
2026-05-15 16:55:20 +0700

    chore: add other files

---
+++ dev/run_dev.R
@@ -0,0 +1,19 @@
+# Set options here
+options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
+
+options(
+  shiny.port = httpuv::randomPort(),
+  shiny.launch.browser = TRUE,
+  error = rlang::entrace,
+  rlang_backtrace_on_error = "full"
+)
+
+# Detach all loaded packages and clean your environment
+golem::detach_all_attached()
+# rm(list=ls(all.names = TRUE))
+
+# Document and reload your package
+golem::document_and_reload()
+
+# Run the application
+run_app()
