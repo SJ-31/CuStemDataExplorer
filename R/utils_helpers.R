c704d53669d88ae17cd3e7c7ed01673a6c41a4e5
Shann Chongwattanankul <shanj3131@gmail.com>
2026-05-15 15:42:53 +0700

    feat: add utility functions for plotting

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -17,3 +17,44 @@ bool_col <- reactable::colDef(
     if (v == "F") "\u274c" else "\u2714\ufe0f"
   }
 )
+
+recode_genes <- function(
+  vec,
+  from = "ensembl",
+  to = "symbol"
+) {
+  valid_formats <- c("ensembl", "symbol", "entrez")
+  checkmate::assert_choice(from, choices = valid_formats)
+  checkmate::assert_choice(to, choices = valid_formats)
+  checkmate::assert_false(from == to)
+  ref <- StemDataExplorer::ensembl_genes_hg38
+  if (from == "ensembl") {
+    names <- ref$ensembl_gene_id
+  } else if (from == "symbol") {
+    names <- hgnc_symbol
+  } else {
+    names <- ref$entrezgene_id
+  }
+  if (to == "ensembl") {
+    vals <- ref$ensembl_gene_id
+  } else if (to == "symbol") {
+    vals <- hgnc_symbol
+  } else {
+    vals <- ref$entrezgene_id
+  }
+  lookup <- setNames(vals, names)
+  mapped <- lookup[vec]
+  ifelse(mapped, is.na(mapped), vec, mapped)
+}
+
+random_palette_d <- function(min_length = NULL) {
+  if (!is.null(length)) {
+    choices <- paletteer::palettes_d_names |> filter(length >= min_length)
+  } else {
+    choices <- paletteer::palettes_d_names
+  }
+  choices |>
+    dplyr::slice_sample(n = 1) |>
+    dplyr::select(package, palette) |>
+    paste0(collapse = "::")
+}
