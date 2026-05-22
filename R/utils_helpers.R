b1aff43bd7f1bcf7f374814febe225ab07ff8abe
Shann Chongwattanankul <shanj3131@gmail.com>
2026-05-22 10:17:04 +0700

    chore: add option to return row from cache

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -141,14 +141,17 @@ add_palette_from_cache <- function(
 #' Retrieve the most recently saved resource `rname` from the
 #' global cache
 #'
-from_bfc <- function(rname) {
+from_bfc <- function(rname, as_row = FALSE) {
   query_res <- BiocFileCache::bfcquery(BFC, rname)
   if (nrow(query_res) > 0) {
-    query_res |>
+    row <- query_res |>
       dplyr::arrange(dplyr::desc(create_time)) |>
-      head(n = 1) |>
-      purrr::pluck("rpath") |>
-      readRDS()
+      head(n = 1)
+    if (as_row) {
+      row
+    } else {
+      readRDS(row$rpath)
+    }
   }
 }
 
