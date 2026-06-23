37376dfed93c93ab2fdfcf800e9f4453300d800a
Shann Chongwattanankul <shanj3131@gmail.com>
2026-06-23 10:25:15 +0700

    chore: begin formatting table for variant display

--- R/utils_exome.R
+++ R/utils_exome.R
@@ -27,6 +27,51 @@ count_sbs <- function(vcf) {
     rename(substitution = "Var1", count = "Freq")
 }
 
+VALID_CONSEQUENCE <- c(
+  "3_prime_UTR_variant",
+  "5_prime_UTR_variant",
+  "coding_sequence_variant",
+  "downstream_gene_variant",
+  "frameshift_variant",
+  "inframe_deletion",
+  "inframe_insertion",
+  "intergenic_variant",
+  "intron_variant",
+  "mature_miRNA_variant",
+  "missense_variant",
+  "NMD_transcript_variant",
+  "non_coding_transcript_exon_variant",
+  "non_coding_transcript_variant",
+  "protein_altering_variant",
+  "regulatory_region_variant",
+  "splice_acceptor_variant",
+  "splice_donor_5th_base_variant",
+  "splice_donor_region_variant",
+  "splice_donor_variant",
+  "splice_polypyrimidine_tract_variant",
+  "splice_region_variant",
+  "start_lost",
+  "start_retained_variant",
+  "stop_gained",
+  "stop_lost",
+  "stop_retained_variant",
+  "synonymous_variant",
+  "upstream_gene_variant"
+)
+
+VALID_CLINSIG <- c(
+  "benign",
+  "likely_benign",
+  "pathogenic",
+  "likely_pathogenic",
+  "not_provided",
+  "other",
+  "",
+  "risk_factor",
+  "uncertain_significance"
+)
+
+
 get_vep_anno <- function(vcf, info_field = "ANN") {
   box::use(VariantAnnotation[info], dplyr[rename, any_of])
   info <- VariantAnnotation::header(vcf) |> info()
@@ -91,9 +136,6 @@ format_sample_vcf <- function(
     "HGVSp",
     "Existing_variation",
     "Canonical",
-    "MANE",
-    "MANE_SELECT",
-    "MANE_PLUS_CLINICAL",
     "HGVSg",
     "ClinSig",
     "Somatic_VEP",
@@ -166,11 +208,23 @@ format_sample_vcf <- function(
   list(tb = tb, sbs_counts = count_sbs(vcf), variant_counts = variant_counts)
 }
 
-combine_vcf_tbs <- function(tbs) {
-  box::use(dplyr[across, any_of])
+filter_list_col <- function(tb, accepted, valid, column, sep = "&") {
+  checkmate::assert_names(accepted, subset.of = valid)
+  checkmate::assert_tibble(tb)
+  if (class(tb[[column]]) != "list") {
+    tb[[column]] <- lapply(
+      tb[[column]],
+      \(cons) stringr::str_split_1(cons, sep)
+    )
+  }
+  mask <- purrr::map_lgl(tb[[column]], \(x) length(intersect(x, accepted)) >= 1)
+  tb[mask, ]
+}
+
+combine_vcf_tbs <- function(tbs, filters = NULL) {
+  box::use(dplyr[across, any_of, mutate, select])
 
   vep_cols <- c(
-    "Consequence",
     "Impact",
     "Symbol",
     "Gene",
@@ -183,9 +237,6 @@ combine_vcf_tbs <- function(tbs) {
     "HGVSp",
     "Existing_variation",
     "Canonical",
-    "MANE",
-    "MANE_SELECT",
-    "MANE_PLUS_CLINICAL",
     "HGVSg",
     "ClinSig",
     "Somatic_VEP",
@@ -196,15 +247,122 @@ combine_vcf_tbs <- function(tbs) {
   avg <- c("AF", "AD_REF", "AD_MAX", "AN", "GERMQ", "QSS", "SomaticEVS")
   uniq <- c("GT", "Sample")
 
+  gene_cols <- c(
+    "Symbol",
+    "Gene",
+    "Feature",
+    "Feature_type",
+    "Biotype",
+    "Canonical"
+  )
+
+  cons_count <- c("synonymous_variant", "missense_variant")
+
   checkmate::assert_list(tbs, names = "unique")
-  lapply(names(tbs), \(x) dplyr::mutate(tbs[[x]], Sample = x)) |>
+  comb <- lapply(names(tbs), \(x) {
+    tb <- dplyr::mutate(
+      tbs[[x]],
+      Sample = x,
+      Consequence = lapply(Consequence, \(cons) str_split_1(cons, "&"))
+    )
+    if ("consequence" %in% names(filters)) {
+      tb <- filter_list_col(
+        tb,
+        filters$consequence,
+        VALID_CONSEQUENCE,
+        "Consequence"
+      )
+    }
+    if ("clinsig" %in% names(filters)) {
+      tb <- filter_list_col(
+        tb,
+        filters$clinsig,
+        VALID_CLINSIG,
+        "ClinSig"
+      )
+    }
+
+    ## for (cons in cons_count) {
+    ##   tb[[paste0("is_", cons)]] <- purrr::pluck(tb$Consequence, \(x) x == cons)
+    ## }
+
+    tb
+  }) |>
     dplyr::bind_rows() |>
-    dplyr::group_by(HGVSg, Consequence) |>
+    dplyr::group_by(HGVSg) |>
     dplyr::summarise(
       `Sample count` = length(unique(Sample)),
       across(any_of(vep_cols), dplyr::first),
       across(any_of(avg), \(x) mean(x, na.rm = TRUE)),
       across(any_of(uniq), \(x) list(unique(x))),
-      SOMATIC = any(SOMATIC)
+      SOMATIC = any(SOMATIC),
+      Consequence = list(unique(unlist(Consequence)))
+    ) |>
+    dplyr::relocate(
+      dplyr::starts_with("HGVS"),
+      .before = dplyr::everything()
+    ) |>
+    dplyr::mutate(
+      HGVSc = stringr::str_extract(HGVSc, ".*c\\.(.*)", group = 1),
+      HGVSp = stringr::str_extract(HGVSp, ".*p\\.(.*)", group = 1)
     )
+
+  grouped <- comb |>
+    group_by(across(all_of(gene_cols))) |>
+    nest() |>
+    mutate(
+      `N variants` = nrow(data),
+      `Consequence counts` = lapply(data, \(x) table(unlist(x$Consequence)))
+    )
+
+  list(genes = grouped, vars = comb)
+}
+
+#' Produce interactive reactable to explore variant data
+#'
+#' @param gene_tb tibble returned from `combine_vcf_tbs$grouped`
+#'
+make_variant_table <- function(gene_tb) {
+  box::use(reactable[reactable, colGroup])
+  reactable(
+    dplyr::select(gene_tb, -data),
+    details = function(index) {
+      cur <- gene_tb$data[[index]]
+      reactable(
+        cur,
+        outlined = TRUE,
+        columnGroups = list(
+          colGroup(
+            name = "IDs",
+            columns = c("HGVSg", "HGVSc", "HGVSp")
+          ),
+          colGroup(name = "Location", columns = c("Exon", "Intron")),
+          colGroup(
+            name = "Classification",
+            columns = c("Consequence", "Impact", "ClinSig"),
+          ),
+          colGroup(
+            name = "Cohort statistics",
+            columns = c(
+              "Sample count",
+              "Sample",
+              "AF",
+              "AD_REF",
+              "AD_MAX",
+              "AN",
+              "QSS",
+              "GERMQ",
+              "SOMATIC",
+              "SomaticEVS",
+              "GT"
+            )
+          ),
+          colGroup(
+            name = "External links",
+            columns = c("Existing_variation", "PubMed")
+          )
+        )
+      )
+    }
+  )
 }
